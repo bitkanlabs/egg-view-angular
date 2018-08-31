9d1c69155349ccfa029deb4fc48b601826fee6a5
Clark Wang <clark@bitkan.com>
2018-08-31 13:03:55 +0800

    feat(locale): add ng-locale

--- app/middleware/ng-locale.js
+++ app/middleware/ng-locale.js
@@ -3,7 +3,13 @@
 module.exports = () => {
   return async function locale(ctx, next) {
     if (ctx.__ngLocale) {
-      ctx.__locale = ctx.__ngLocale();
+      const locale = ctx.__ngLocale();
+
+      if (locale && ctx.url.startsWith(`/${locale}/`)) {
+        ctx.url = ctx.url.substring(locale.length + 1);
+      }
+
+      ctx.__locale = locale;
     }
     await next();
   };
