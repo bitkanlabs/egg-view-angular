a7f8732adefddbf81291abdc961fdef983219d9d
Clark Wang <clark@bitkan.com>
2018-08-31 15:59:22 +0800

    fix(locale): convert array to object

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 module.exports = app => {
   app.view.use('angular', require('./lib/view'));
-  require('./lib/ng-locale')(app, app.config.angular);
+  require('./lib/ngi18n')(app, app.config.angular);
 
   // 自动加载 Middleware
   app.config.coreMiddleware.push('ngi18n');
