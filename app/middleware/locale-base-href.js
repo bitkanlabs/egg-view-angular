e1d9ba4ccf3573d4fe4db799d7f1ce6a869bcfc8
Clark Wang <clark@bitkan.com>
2018-09-05 18:50:39 +0800

    fix(ctx): keys

--- app/middleware/locale-base-href.js
+++ app/middleware/locale-base-href.js
@@ -2,7 +2,7 @@
 
 module.exports = () => {
   return async function localeBaseHref(ctx, next) {
-    if (ctx.locales.keys().length) {
+    if (Object.keys(ctx.locales || {}).length) {
       const match = /^\/([a-z]{2}(?:-[A-Z]{2})?)(\/.*)/gi.exec(this.url);
       if (match && ctx.locales[match[1]]) {
         Object.assign(ctx, match && { __locale: match[1], url: match[2] } || {});
