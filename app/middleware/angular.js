fb0d6fdd5000d80409860814c8397d4fc4056704
Clark Wang <clark@bitkan.com>
2018-09-05 22:20:30 +0800

    fix(middleware): fix undefined

--- app/middleware/angular.js
+++ app/middleware/angular.js
@@ -2,7 +2,7 @@
 
 module.exports = () => {
   return async function angular(ctx, next) {
-    ctx.body = await ctx.angular.render(ctx);
+    ctx.body = await ctx.app.angular.render(ctx);
     await next();
   };
 };
