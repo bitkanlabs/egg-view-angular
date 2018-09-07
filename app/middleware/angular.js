9d65d5f8aa427907e29972900ceba122d9567055
Clark Wang <clark@bitkan.com>
2018-09-07 12:36:26 +0800

    fix(types): restrict to handle html type only

--- app/middleware/angular.js
+++ app/middleware/angular.js
@@ -2,7 +2,9 @@
 
 module.exports = () => {
   return async function angular(ctx, next) {
-    ctx.body = await ctx.app.angular.render(ctx);
+    if (ctx.accept.type().indexOf('text/html') === 0) {
+      ctx.body = await ctx.app.angular.render(ctx);
+    }
     await next();
   };
 };
--- app/middleware/locale-base-href.js
+++ app/middleware/locale-base-href.js
@@ -2,7 +2,7 @@
 
 module.exports = () => {
   return async function localeBaseHref(ctx, next) {
-    if (Object.keys(ctx.locales || {}).length) {
+    if (ctx.accept.type().indexOf('text/html') === 0 && Object.keys(ctx.locales || {}).length) {
       const match = /^\/([a-z]{2}(?:-[A-Z]{2})?)(\/.*)/gi.exec(ctx.url);
       if (match && ctx.locales[match[1]]) {
         ctx.url = match[2];
